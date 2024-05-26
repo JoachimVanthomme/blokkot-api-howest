68d56a1d12c8e61083c34a194ce50e455d339e45
VanthommeJoachim <joachim.vanthomme@student.howest.be>
2024-05-26 23:35:48 +0200

    API routes cleaning

--- routes/api.php
+++ routes/api.php
@@ -7,57 +7,35 @@ use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
-Route::middleware(['auth:sanctum'])->get('/user', function () {
+Route::get("/images/{image_path}", [LocationController::class, "findImageByName"]); //get an image by name
+Route::get("/cities", [LocationController::class, "allCities"]);
+Route::get("/locations/{id}", [LocationController::class, "find"])->middleware(['language']);
+Route::get("/locations-city/{city}", [LocationController::class, "findByCity"]);
+
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return auth()->user();
 });
 
-Route::get("/images/{image_path}", [LocationController::class, "findImageByName"]); //get an image by name
-
-Route::get("/locations/most-locations", [LocationController::class, "mostLocations"]);
-
-Route::get("/cities", [LocationController::class, "allCities"]);
-
-Route::get('locations/languages/{id}', [LocationController::class, 'getAllLanguages']);
-
-//Route::middleware(['auth:sanctum','verified', 'language'])->group(function () {
-Route::middleware(['language'])->group(function () {
-    //Locations routes
-    Route::get("/locations", [LocationController::class, "all"])->middleware(['isAdmin']); //only show language dependant data on detail request
-    Route::get("/locations/{id}", [LocationController::class, "find"]);
-
-    Route::post("/locations", [LocationController::class, "add"])->middleware(['isAdmin']);
-    //Route::post("/locations", [LocationController::class, "add"]);
-
-    Route::put("/locations/{id}", [LocationController::class, "update"]);
-
-    Route::delete("/locations/{id}", [LocationController::class, "delete"]);
-
-    Route::get("/locations-city/{city}", [LocationController::class, "findByCity"]);
-
-    //Favourites routes
+Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('/favourites', [FavouriteController::class, 'findByUser']);
+
     Route::post('/favourites/{location_id}', [FavouriteController::class, 'add']);
     Route::delete('/favourites/{location_id}', [FavouriteController::class, 'delete']);
-
-    //Owners routes
-    Route::get('/owners', [OwnerController::class, 'findByUser'])->middleware(['isAdmin']);
-    //Route::post('/owners', [OwnerController::class, 'add'])->middleware(['isDeveloper']);
-    Route::delete('/owners/{location_id}', [OwnerController::class, 'delete'])->middleware(['isAdmin']);
 });
 
-//Users routes (FOR DEV ONLY)
-Route::get('/users', [UserController::class, 'all'])->middleware(['isDeveloper']);
-Route::get('/admins', [UserController::class, 'admins'])->middleware(['isDeveloper']);
-Route::get('/developers', [UserController::class, 'developers'])->middleware(['isDeveloper']);
+Route::middleware(['isAdmin'])->group(function () {
+    Route::get("/locations", [LocationController::class, "all"]);
+    Route::get('locations/languages/{id}', [LocationController::class, 'getAllLanguages']);
+    Route::post("/locations", [LocationController::class, "add"]);
+    Route::put("/locations/{id}", [LocationController::class, "update"]);
+    Route::delete("/locations/{id}", [LocationController::class, "delete"]);
 
+    Route::get('/owners', [OwnerController::class, 'findByUser']);
+});
+
+Route::middleware(['isDeveloper'])->group(function () {
+    Route::get('/users', [UserController::class, 'all']);
+    Route::get('/admins', [UserController::class, 'admins']);
+    Route::get('/developers', [UserController::class, 'developers']);
+});
